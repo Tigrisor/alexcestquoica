9d35a84f7d8d936862656e9f1393f7e33fb245d6
jdemay <taaroa23@gmail.com>
2016-08-30 12:56:11 +0200

    migration du script updatefilelist en php

--- updateFileLists.php
+++ updateFileLists.php
@@ -1,13 +1,38 @@
 <?php
+
+    function surroundWithQuotes($stringToSurround){
+		return "\"".$stringToSurround."\"";
+    }
+
+
 //$listFiles = scandir ( "jstree-v.pre1.0");
 //echo $listFiles;
 
 $imageDirectory="image";
+
 $slash="/";
+$accOuv="{";
+$accFer="}";
+$bracOuv="[";
+$bracFer="]";
+$dirString="\"directory\":";
+$filesString="\"files\":";
+$pathString="\"path\":";
+$nameString="\"name\":";
+$comma=",";
+
+$result="fileList='[";
 
 $subDirectoryList = array_diff(scandir($imageDirectory), array('..', '.'));
 
+
+$numSubDirectory = count($subDirectoryList);
+$i = 0;
+
 foreach ($subDirectoryList as $subDirectory) {
+	
+	$result=$result.$accOuv.$dirString.surroundWithQuotes($subDirectory).$comma.$filesString.$bracOuv;
+	
     //echo $subDirectory;    
 	$subfilePath = $imageDirectory.$slash.$subDirectory;
 	//echo $subfilePath;
@@ -15,13 +40,44 @@ foreach ($subDirectoryList as $subDirectory) {
 	if(is_dir( $subfilePath ))
 	{
 		$fileList = array_diff(scandir($subfilePath), array('..', '.'));
+		
+		$numFile = count($fileList);
+		$j = 0;
 		foreach ($fileList as $file) {
-			echo $subfilePath.$slash.$file;
-			echo "<br>";
+			$result=$result.$accOuv.$pathString.surroundWithQuotes($subfilePath.$slash.$file).$comma.$nameString.surroundWithQuotes($subDirectory).$accFer;
+			
+			
+			if(++$j !== $numFile) {
+				$result=$result.$comma;
+				}
+			
+			//echo $subfilePath.$slash.$file;
+			//echo "<br>";
 		}
 	}
+	//echo "<br>";
 	
-	
-	echo "<br>";
-} 
+	$result=$result.$bracFer.$accFer;
+		if(++$i !== $numSubDirectory) {
+		$result=$result.$comma;
+		}
+}
+
+
+
+$result=$result."]'";
+echo $result;
+
+$writeResult = file_put_contents( "scripts/list_files.json" , $result );
+
+if($writeResult != 0)
+{
+	echo "La liste de fichier a été mise à jour."
+}
+else
+{
+	echo "Erreur lors de l'écriture du fichier de liste d'image."
+}
+
+
 ?>
